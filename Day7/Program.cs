138cd53434fb49c8f5dd7511aa4343c36b124372
runarer <95697611+runarer@users.noreply.github.com>
2025-11-08 17:07:33 +0100

    Solves part 2 and part 1

--- Day7/Program.cs
+++ Day7/Program.cs
@@ -9,36 +9,63 @@
     return (result.Groups[1].Value, int.Parse(result.Groups[2].Value), [.. result.Groups[3].Value.Split([' ', ','], StringSplitOptions.RemoveEmptyEntries)]);
 }
 
+//string Part1(List<string> lines)
+//{
+//    HashSet<string> programs = [];
+//    HashSet<string> linkedPrograms = [];
 
-string Part1(List<string> lines)
+//    foreach (string line in lines)
+//    {
+//        var (program, _, linked) = ParseLine(line);
+//        programs.Add(program);
+//        linkedPrograms.UnionWith(linked);
+//    }
+
+//    return programs.Except(linkedPrograms).First();
+//}
+
+ProgramNode BuildTree(List<string> lines)
 {
-    HashSet<string> programs = [];
+    Dictionary<string, ProgramNode> programs = [];
     HashSet<string> linkedPrograms = [];
+    List<KeyValuePair<string, string[]>> parentChildren = [];
 
+    // Parse lines and create nodes
     foreach (string line in lines)
     {
-        var (program, _, linked) = ParseLine(line);
-        programs.Add(program);
+        var (program, weight, linked) = ParseLine(line);
+        programs[program] = new ProgramNode(program, weight);
+        parentChildren.Add(new KeyValuePair<string, string[]>(program, linked));
         linkedPrograms.UnionWith(linked);
     }
 
-    return programs.Except(linkedPrograms).First();
-}
-
-int? Part2(List<string> lines, string root)
-{
-    Dictionary<string, int> programs = [];
-    Dictionary<string, string[]> links = [];
-
-    foreach (string line in lines)
+    // Build tree
+    foreach (var (parent, children) in parentChildren)
     {
-        var (program, weight, link) = ParseLine(line);
-        programs[program] = weight;
-        if (link != null)
-            links[program] = link;
+        foreach (var child in children)
+        {
+            programs[parent].AddLink(programs[child]);
+            programs[child].AddParent(programs[parent]);
+        }
     }
 
-    return null;
+    // Find root
+    ProgramNode root = programs[programs.Keys.Except(linkedPrograms).First()];
+
+    root.UpdateWeights();
+
+    return root;
+}
+
+int? Part2(ProgramNode root)
+{
+    foreach( ProgramNode child in root.SubPrograms)
+    {
+        if (child.TotalWeight != root.CorrectWeight)
+            return Part2(child);
+    }
+
+    return root.Weight + (root.Parent.CorrectWeight - root.TotalWeight);
 }
 
 
@@ -55,6 +82,49 @@ using (StreamReader sr = new(args[0]))
     }
 }
 
-string root = Part1(lines);
-Console.WriteLine("Part 1: {0}", root);
-Console.WriteLine("Part 2: {0}", Part2(lines, root));
+ProgramNode root = BuildTree(lines);
+Console.WriteLine("Part 1: {0}", root.Name);
+Console.WriteLine("Part 2: {0}", Part2(root));
+
+
+class ProgramNode(string name, int weight)
+{
+    public string Name { get; private set; } = name;
+    public int Weight { get; private set; } = weight;
+    public int TotalWeight { get; private set; } = weight;
+    public List<ProgramNode> SubPrograms { get; private set; } = new List<ProgramNode>();
+    public ProgramNode? Parent { get; private set; } = null;
+    public int CorrectWeight { get; private set; } = weight;
+
+    public void AddLink(ProgramNode linked)
+    {
+        SubPrograms.Add(linked);
+    }
+
+    public void AddParent(ProgramNode parent)
+    {
+        Parent = parent;
+    }
+
+    public int UpdateWeights()
+    {
+        TotalWeight += SubPrograms.Sum(x => x.UpdateWeights());
+        if (SubPrograms.Count == 2)
+        {
+            CorrectWeight = SubPrograms[0].TotalWeight;
+        }
+        else if (SubPrograms.Count > 2)
+        {
+            if (SubPrograms[0].TotalWeight == SubPrograms[1].TotalWeight)
+            {
+                CorrectWeight = SubPrograms[0].TotalWeight;
+            }
+            else
+            {
+                CorrectWeight = SubPrograms[2].TotalWeight;
+            }
+        }
+
+        return TotalWeight;
+    }
+}
