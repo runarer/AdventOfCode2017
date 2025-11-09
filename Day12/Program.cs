1e82d577fa0a7d12c4cad31aee985a9301b55d87
runarer <95697611+runarer@users.noreply.github.com>
2025-11-09 18:30:10 +0100

    Solves day 12 part 1 and part 2

---
+++ Day12/Program.cs
@@ -0,0 +1,62 @@
+﻿int[][] lines = [];
+string[] deliminators = ["<->", ","];
+try
+{
+    lines = [..File.ReadAllLines(args[0])
+                   .Select(x => x.Split(deliminators, StringSplitOptions.RemoveEmptyEntries)
+                                 .Skip(1)
+                                 .Select(int.Parse)
+                                 .ToArray()
+                           )
+            ];
+}
+catch (Exception ex)
+{
+    Console.WriteLine(ex.Message);
+    return 1;
+}
+
+
+Console.WriteLine($"Part 1: {Part1(lines)}");
+Console.WriteLine($"Part 1: {Part2(lines)}");
+return 0;
+
+void FindGroup(int[][] lines, int start, bool[] visited)
+{
+    Queue<int> queue = new();
+
+    queue.Enqueue(start);
+
+    while (queue.Count > 0)
+    {
+        int current = queue.Dequeue();
+        visited[current] = true;
+
+        foreach (int next in lines[current])
+        {
+            if (visited[next]) continue;
+            queue.Enqueue(next);
+        }
+    }
+}
+int? Part1(int[][] lines) {
+    bool[] visited = new bool[lines.Length];
+    FindGroup(lines, 0, visited);
+    return visited.Count(x => x);
+}
+int? Part2(int[][] lines)
+{
+    bool[] visited = new bool[lines.Length];
+    int groups = 0;
+
+    for (int i = 0; i < lines.Length; i++)
+    {
+        if (visited[i]) continue;
+        FindGroup(lines, i, visited);
+        groups++;
+    }
+
+    return groups;
+}
+
+
