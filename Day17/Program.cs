27fc0181c3ce17178fb349afb8cdd0de1ec4ef66
runarer <95697611+runarer@users.noreply.github.com>
2025-11-16 12:19:08 +0100

    Simplifies a brute force method, still to slow

--- Day17/Program.cs
+++ Day17/Program.cs
@@ -17,24 +17,16 @@ catch (Exception ex)
     return 1;
 }
 
-
-
-
-/* TEST */
-//steps = 3;
-
-Console.WriteLine($"Part 1: {Part1(steps,2018)}");
-Console.WriteLine($"Part 1: {Part2(steps,50_000_000)}");
+Console.WriteLine($"Part 1: {Solution(steps,2018,2017)}");
+Console.WriteLine($"Part 1: {Solution(steps,50_000_000,0)}");
 return 0;
 
-
-int Part1(int steps, int ValuesToAdd)
+static int Solution(int steps, int ValuesToAdd,int after)
 {
-    int[,] list = new int[ValuesToAdd, 2];
-    list[0, 0] = 1;
-    list[0, 1] = 1;
-    list[1, 0] = 0;
-    list[1, 1] = 0;
+    
+    int[] list = new int[ValuesToAdd];    
+    list[0] = 1;
+    list[1] = 0;
     int currentNode = 1;
     int currentSize = 2;
 
@@ -43,130 +35,15 @@ int Part1(int steps, int ValuesToAdd)
         int movesForward = steps % currentSize;
         while (movesForward > 0)
         {
-            currentNode = list[currentNode, 1];
+            currentNode = list[currentNode];
             movesForward--;
         }
 
-        list[currentSize, 0] = currentNode;
-        list[currentSize, 1] = list[currentNode, 1];
-        list[list[currentSize, 0], 1] = currentSize;
-        list[list[currentSize, 1], 0] = currentSize;
+        list[currentSize] = list[currentNode];
+        list[currentNode] = currentSize;
         currentNode = currentSize;
         currentSize++;        
     }
-
-    return list[2017, 1];
+        
+    return list[after];
 }
-
-int Part2(int steps,int ValuesToAdd)
-{
-    int[,] list = new int[ValuesToAdd, 2];
-    list[0, 0] = 1;
-    list[0, 1] = 1;
-    list[1, 0] = 0;
-    list[1, 1] = 0;
-    int currentNode = 1;
-    int currentSize = 2;
-
-    while (currentSize < ValuesToAdd)
-    {
-        int movesForward = steps % currentSize;
-        while (movesForward > 0)
-        {
-            currentNode = list[currentNode, 1];
-            movesForward--;
-        }
-
-        list[currentSize, 0] = currentNode;
-        list[currentSize, 1] = list[currentNode, 1];
-        list[list[currentSize, 0], 1] = currentSize;
-        list[list[currentSize, 1], 0] = currentSize;
-        currentNode = currentSize;
-        currentSize++;
-    }
-
-    return list[0, 1];
-}
-
-// This is brute force and not a good solution.
-
-//int? Part1(int steps)
-//{
-//    RoundList rl = new(steps);
-//    while (rl.Count < ValuesToAdd)
-//    {
-//        rl.AddValue();
-//    }
-
-//    return rl.PeakAtNext();
-//}
-
-
-// Takes close to four minutes to finish.
-//int? Part2(int steps)
-//{
-//    RoundList rl = new(steps);
-//    while (rl.Count < 50_000_000)
-//    {
-//        rl.AddValue();
-//    }
-
-//    return rl.Root.Next.Value;
-//}
-
-//class RoundList
-//{
-
-//    public int Count { get; set; } = 1;
-//    public Node Root = new(0);
-
-//    private Node _currentNode;
-//    private int _currentValue = 0;
-//    private int _steps = 0;
-
-//    public RoundList(int steps)
-//    {
-//        _steps = steps;
-//        _currentNode = Root;
-//        _currentNode.Next = new(1);
-//        _currentNode.Prev = _currentNode.Next;
-//        _currentNode.Next.Next = _currentNode;
-//        _currentNode.Next.Prev = _currentNode;
-//        _currentNode = _currentNode.Next;
-//        _currentValue = 1;
-//        Count++;
-//    }
-
-//    public void AddValue()
-//    {
-//        int movesForward = _steps % Count;
-//        while (movesForward > 0)
-//        {
-//            movesForward--;
-//            _currentNode = _currentNode.Next;
-//        }
-
-//        Count++;
-//        _currentValue++;
-//        Node newNode = new(_currentValue);
-//        newNode.Next = _currentNode.Next;
-//        newNode.Prev = _currentNode;
-//        _currentNode = newNode;
-//        newNode.Next.Prev = newNode;
-//        newNode.Prev.Next = newNode;
-//    }
-
-//    public int PeakAtNext()
-//    {
-//        if (_currentNode.Next == null) throw new Exception("Missing Next node!");
-//        return _currentNode.Next.Value;
-//    }
-//}
-
-//class Node(int value)
-//{
-//    public int Value { get; private set; } = value;
-//    public Node? Next { get; set; } = null;
-//    public Node? Prev { get; set; } = null;
-
-//}
