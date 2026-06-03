3dc583508c496e58478fdbad0fc36de3ca717525
runarer <95697611+runarer@users.noreply.github.com>
2026-06-03 09:50:56 +0200

    Start Day 21

---
+++ Day21/Program.cs
@@ -0,0 +1,32 @@
+﻿
+string[] lines;
+
+(int[], int[]) Parse2x2Rule(string line)
+{
+    List<int> top = [];
+    List<int> bottom = [];
+
+    // ^([\.#]{2})\/([\.#]{2}) => ([\.#]{3})\/([\.#]{3})\/([\.#]{3})
+}
+
+using (StreamReader reader = new(args[0]))
+{
+    try
+    {
+        string valueA = reader.ReadToEnd();
+        lines = valueA.Split(['\n']);
+    }
+    catch (Exception ex)
+    {
+        Console.WriteLine(ex.ToString());
+        return 1;
+    }
+}
+
+
+        
+var twoByTwoLines = lines.Take(6).Select().ToArray();
+var threeByThreeLines = lines.Skip(6).ToArray();
+
+
+return 0;
