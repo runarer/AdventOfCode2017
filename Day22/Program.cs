f48b66d536ce8c246fa67c8b6d51a0f642944cc3
runarer <95697611+runarer@users.noreply.github.com>
2026-09-22 14:47:18 +0200

    Solves part 1 day 22 2017

---
+++ Day22/Program.cs
@@ -0,0 +1,116 @@
+﻿string[] lines;
+
+using (StreamReader reader = new(args[0]))
+{
+    try
+    {
+        string valueA = reader.ReadToEnd();
+        lines = [..valueA.Split(['\n']).Where(line => !string.IsNullOrEmpty(line)).Select(line =>line.Trim())];
+    }
+    catch (Exception ex)
+    {
+        Console.WriteLine(ex.ToString());
+        return 1;
+    }
+}
+
+int mapWidth = lines[0].Length;
+int mapHeight = lines.Length; // We have an empty line at the end.
+
+Console.WriteLine($"H: {mapHeight} W: {mapWidth}");
+
+// The map is infinite so we need to keep track of infeced squares and not the whole map.
+// It's the only information we need.
+List<(int, int)> infected = [];
+
+
+// The virus starts in the center so we need to orient the map after that.
+// Center is 0,0
+int y = (mapHeight - 1) / 2;
+for (int row = 0; row < mapHeight; row++,y--)
+{
+    int x = -1 * ((mapWidth - 1) / 2);
+    for (int col = 0; col < mapWidth; col++,x++)
+    {
+        if (lines[row][col] == '#')
+            infected.Add((x, y));
+    }
+}
+
+int infections = runBurst(10_000);
+Console.WriteLine($"Part 1 {infections}");
+
+return 0;
+
+
+// The virus moves in burst so lets simulate those
+int runBurst(int bursts)
+{
+    List<(int, int)> tempInfected = [.. infected];
+    (int, int) currentNode = (0, 0);
+    Direction currentDirection = Direction.Up;
+    int infections = 0;
+
+    for (int burst = 0; burst < 10_000; burst++)
+    {
+        if (tempInfected.Contains(currentNode))
+        {
+            currentDirection = turnRight(currentDirection);
+            tempInfected.Remove(currentNode);
+        }
+        else
+        {
+            currentDirection = turnLeft(currentDirection);
+            tempInfected.Add(currentNode);
+            infections++;
+        }
+        currentNode = move(currentNode.Item1, currentNode.Item2, currentDirection);
+    }
+    return infections;
+}
+
+static (int, int) move(int x, int y, Direction direction) => direction switch
+{
+    Direction.Up => (x, y + 1),
+    Direction.Down => (x, y - 1),
+    Direction.Left => (x-1, y),
+    Direction.Right => (x+1, y),
+    _ => throw new NotImplementedException()
+};
+
+static Direction turnLeft(Direction direction) => direction switch
+{
+    Direction.Up => Direction.Left,
+    Direction.Left => Direction.Down,
+    Direction.Down => Direction.Right,
+    Direction.Right => Direction.Up,
+    _ => throw new NotImplementedException()
+};
+
+static Direction turnRight(Direction direction) => direction switch
+{
+    Direction.Up => Direction.Right,
+    Direction.Left => Direction.Up,
+    Direction.Down => Direction.Left,
+    Direction.Right => Direction.Down,
+    _ => throw new NotImplementedException()
+};
+
+NodeStatus ChangeStatus(NodeStatus status) => status switch
+{
+    NodeStatus.Clean => NodeStatus.Weakened,
+    NodeStatus.Weakened => NodeStatus.Infected,
+    NodeStatus.Infected => NodeStatus.Flagged,
+    NodeStatus.Flagged => NodeStatus.Clean,
+    _ => throw new NotImplementedException()
+};
+
+enum Direction
+{
+    Up, Down, Left, Right,
+}
+
+enum NodeStatus
+{
+    Clean, Weakened, Flagged, Infected,
+}
